0525bb31f2b66c075e0b2d29cc2dd8b91f43f9e2
Eduardo Villagomez <eddvillagomez1@gmail.com>
2026-02-17 12:43:45 -0500

    Create contest.py

---
+++ contest.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture
+def driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--start-maximized")
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=chrome_options
+    )
+
+    driver.get("https://mike-borges.github.io/TimeKeeper/")
+    yield driver
+
+    driver.quit()
